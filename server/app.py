270a5bf7893c34f3253294039efca9b07d1b2910
Arnab Koley <arnab123koley@gmail.com>
2024-11-02 09:50:46 +0530

    error fixed

--- server/app.py
+++ server/app.py
@@ -8,7 +8,7 @@ import io
 from PIL import Image
 
 app = Flask(__name__)
-CORS(app,origins=["http://localhost:5173", "https://cancer-one.vercel.app/"])
+CORS(app,origins=["http://localhost:5173", "https://cancer-one.vercel.app"])
 
 # Load the saved model
 model = load_model('MobileNetV2.keras')
